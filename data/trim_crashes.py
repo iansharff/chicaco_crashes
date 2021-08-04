430305e6c8f94d66ea902bd0d7ed58be7a35a2cf
iansharff <ips11@georgetown.edu>
2021-08-03 23:24:54 -0400

    Add trim_crashes.py
    This can be executed from a terminal session to reload the PKL files from
    the large CSVs that will not be included in the repository. Unless the CSVs
    are present in the data/ directory, this script will fail.

---
+++ data/trim_crashes.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+import pickle
+
+CRASHES_DROP = [
+    'RD_NO',
+    'CRASH_DATE_EST_I',
+    'LANE_CNT',
+    'REPORT_TYPE',
+    'NOT_RIGHT_OF_WAY_I',
+    'DATE_POLICE_NOTIFIED',
+    'SEC_CONTRIBUTORY_CAUSE',
+    'STREET_NO',
+    'STREET_DIRECTION',
+    'STREET_NAME',
+    'BEAT_OF_OCCURRENCE',
+    'PHOTOS_TAKEN_I',
+    'STATEMENTS_TAKEN_I',
+    'DOORING_I',
+    'WORK_ZONE_TYPE',
+    'WORKERS_PRESENT_I',
+    'INJURIES_UNKNOWN',
+    'INJURIES_INCAPACITATING',
+    'INJURIES_NON_INCAPACITATING',
+    'INJURIES_REPORTED_NOT_EVIDENT',
+    'INJURIES_NO_INDICATION',
+    'LOCATION'
+]
+
+NA_VALUES = [
+    'UNKNOWN',
+    'OTHER',
+    'NOT APPLICABLE',
+    'UNABLE TO DETERMINE',
+    'NOT REPORTED',
+]
+
+
+def main():
+    crashes = pd.read_csv('chicago_crashes.csv',
+                          na_values=NA_VALUES,
+                          true_values=['Y'],
+                          false_values=['N']
+                          ).drop(columns=CRASHES_DROP)
+
+    # Save only samples that have a clear primary contributory cause
+    crashes.dropna(subset=['PRIM_CONTRIBUTORY_CAUSE'], axis=0, inplace=True)
+
+    # Change CRASH_DATE to CRASH_YEAR
+    crashes['CRASH_DATE'] = crashes['CRASH_DATE'].map(lambda x: pd.to_datetime(x).year)
+    crashes.rename({'CRASH_DATE': 'CRASH_YEAR'})
+
+    crashes_injuries = crashes[crashes.MOST_SEVERE_INJURY.isin([
+        'NONINCAPACITATING INJURY',
+        'INCAPACITATING INJURY',
+        'FATAL'
+    ])]
+    print(crashes.info())
+
+    with open('crashes.pkl', 'wb') as c:
+        pd.to_pickle(crashes, c)
+
+    with open('crashes_injuries.pkl', 'wb') as ci:
+        pd.to_pickle(crashes_injuries, ci)
+
+
+if __name__ == '__main__':
+    main()
