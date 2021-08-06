811a29d0f38eb382407a0e452e1f52a577d77920
iansharff <ips11@georgetown.edu>
2021-08-06 15:48:20 -0400

    Add comments to bin_causes.py

--- bin_causes.py
+++ bin_causes.py
@@ -1,17 +1,20 @@
 import pickle
-
+"""
+This script is for determining the aggregated target values in 'PRIM_CONTRIBUTORY_CAUSE' and saving them as
+a PKL file to be used for the modeling notebooks. It can be run in the terminal from the root directory.
+"""
 
 def main():
     driving = [
-        'FAILING TO YIELD RIGHT-OF-WAY', # inattention to cars
-        'FOLLOWING TOO CLOSELY', # driver practices
-        'IMPROPER OVERTAKING/PASSING', # inattention to cars
-        'FAILING TO REDUCE SPEED TO AVOID CRASH', # inattention to cars
-        'IMPROPER BACKING', # inattention to cars
-        'IMPROPER LANE USAGE', # innatention to environment
-        'IMPROPER TURNING/NO SIGNAL', # driving practices
-        'DRIVING SKILLS/KNOWLEDGE/EXPERIENCE', # driving practices
-        'OPERATING VEHICLE IN ERRATIC, RECKLESS, CARELESS, NEGLIGENT OR AGGRESSIVE MANNER', # driving practices
+        'FAILING TO YIELD RIGHT-OF-WAY',
+        'FOLLOWING TOO CLOSELY',
+        'IMPROPER OVERTAKING/PASSING',
+        'FAILING TO REDUCE SPEED TO AVOID CRASH',
+        'IMPROPER BACKING',
+        'IMPROPER LANE USAGE',
+        'IMPROPER TURNING/NO SIGNAL',
+        'DRIVING SKILLS/KNOWLEDGE/EXPERIENCE',
+        'OPERATING VEHICLE IN ERRATIC, RECKLESS, CARELESS, NEGLIGENT OR AGGRESSIVE MANNER',
         'PHYSICAL CONDITION OF DRIVER',
         'UNDER THE INFLUENCE OF ALCOHOL/DRUGS (USE WHEN ARREST IS EFFECTED)',
         'DRIVING ON WRONG SIDE/WRONG WAY',
